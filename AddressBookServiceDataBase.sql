9b030c66803481deb5d8cf61a116c9d5b48e9e76
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-25 21:48:56 +0530

    [VIGNESH M] Added: INSERT INTO to add values for contact details

--- AddressBookServiceDataBase.sql
+++ AddressBookServiceDataBase.sql
@@ -12,3 +12,8 @@ Zip int,
 PhoneNumber bigint,
 Email varchar(35)
 );
+
+-----UC3-InsertNewContact-----
+INSERT INTO addressbookservice_table(FirstName,LastName,Address,City,States,Zip,PhoneNumber,Email) VALUES('Vignesh','Marimuthu','HAPP','Trichy','Tamil Nadu',620025,9647812357,'vicky@gmail.com');
+INSERT INTO addressbookservice_table(FirstName,LastName,Address,City,States,Zip,PhoneNumber,Email) VALUES('Praveen','Muthu','NGO Colony','Dindigul','Tamil Nadu',624005,7458961235,'praveen@gmail.com');
+select * from addressbookservice_table;
