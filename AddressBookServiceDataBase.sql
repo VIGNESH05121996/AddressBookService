ca08e8fa5b0ef12898a4e54c7a18bdfaca872ac3
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-11-25 21:40:07 +0530

    [VIGNESH M] Added: INSERT INTO to add values for contact details

--- AddressBookServiceDataBase.sql
+++ AddressBookServiceDataBase.sql
@@ -1,2 +1,14 @@
 -----UC1-CreateDataBase-----
 create database address_book_service;
+
+-----UC2-CreateTable-----
+CREATE TABLE addressbookservice_table(
+FirstName varchar(35),
+LastName varchar(35),
+Address varchar(100),
+City varchar(35),
+States varchar(35),
+Zip int,
+PhoneNumber bigint,
+Email varchar(35)
+);
